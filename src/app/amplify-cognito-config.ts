bdafe781aa2e418c59b183470c6e7e4a086d53e4
Majd Mousa <majdmousa@Majds-MacBook-Pro-2.local>
2024-06-24 21:22:18 +0300

    change path

--- src/app/amplify-cognito-config.ts
+++ src/app/amplify-cognito-config.ts
@@ -1,8 +1,8 @@
 "use client";
+import awsmobile from "@/aws-exports";
 import { Amplify } from "aws-amplify";
-import config from "../aws-exports";
 
-Amplify.configure(config, { ssr: true });
+Amplify.configure(awsmobile, { ssr: true });
 
 export default function ConfigureAmplifyClientSide() {
   return null;
